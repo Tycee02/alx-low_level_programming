32559fb4f5ce63e7bd44c34772ea2447253891c1
Tycee02 <wakilaminat@gmail.com>
2023-09-13 01:13:50 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - detect lowercase and uppercase
+ * Return: is 1
+ */
+int _islower(int c)
+{
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+		{
+		lower = 1;
+		}
+	}
+	return (lower);
+}
