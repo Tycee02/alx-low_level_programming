63e556bcfdc08ba489d2bda635e2931737d531e3
Tycee02 <wakilaminat@gmail.com>
2023-10-04 11:20:38 -0700

    4-free_grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * free_grid - free memory previously allocated
+ * @grid: pointer to the array
+ * @height: numbers of rows
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
