f3b57f0ab29b9050544631bf6dacdd246b3f79b6
Tycee02 <wakilaminat@gmail.com>
2023-10-07 07:53:34 -0700

    0-malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * malloc_checked -  allocates memory
+ * @b: size of memory
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *output;
+
+	output = malloc(b);
+	if (output == NULL)
+		exit(98);
+	else
+		return (output);
+}
