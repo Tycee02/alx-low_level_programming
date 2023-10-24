e4c767cf349bf7be80dd1e3e8dfa94ee01b64fff
Tycee02 <wakilaminat@gmail.com>
2023-10-24 12:12:47 -0700

    function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,44 +1,63 @@
 #include "lists.h"
-
 /**
- * insert_nodeint_at_index - insert a new node at a given point
- * @head: pointer to first node
- * @idx: index of the node
- * @n: integer number
- * Return: address of new node
+ * create_new_node - creates a new node
+ * @n: the data of the node
+ * Return: pointer to the node
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *create_new_node(int n)
 {
-	unsigned int j;
 	listint_t *newnode;
-	listint_t *h;
-
-	h = *head;
-
-	if (idx != 0)
-	{
-		for (j = 0; j < idx - 1 && h != NULL; j++)
-		{
-			h = h->next;
-		}
-	}
-	if (h == NULL && idx != 0)
-		return (NULL);
 
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
-
 	newnode->n = n;
+	newnode->next = NULL;
+
+	return (newnode);
+}
+
+/**
+ * insert_nodeint_at_index - insert a node at an index in a list
+ * @head: pointer to the first element of the list
+ * @idx: index to the position the new node is to be inserted
+ * @n: the data of the new node created
+ * Return: the address of the newly created node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *tempo;
+	listint_t *tempo_old;
+	listint_t *newnode;
+
+	tempo = *head;
+	if (head == NULL)
+		return (NULL);
+	newnode = create_new_node(n);
+	if (newnode == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (newnode);
+	}
 
 	if (idx == 0)
 	{
-		newnode->next = *head;
+		*head = newnode;
 	}
+	for (i = 0; i < idx - 1 && tempo != NULL && idx != 0; i++)
+		tempo = tempo->next;
+	if (tempo == NULL)
+		return (NULL);
+	if (idx == 0)
+		newnode->next = tempo;
 	else
 	{
-		newnode->next = h->next;
-		h->next = newnode;
+		tempo_old = tempo->next;
+		tempo->next = newnode;
+		newnode->next = tempo_old;
 	}
 	return (newnode);
 }
