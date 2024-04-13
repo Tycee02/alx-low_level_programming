9bf3a89cd286dde7fa8720fe49987bf8d8be164d
Tycee02 <wakilaminat@gmail.com>
2024-04-13 18:33:28 +0100

    This is a header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,41 @@
+#ifndef "SEARCH_ALGOS.H"
+#define "SEARCH_ALGOS.H"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list node structure
+ * for ALX project
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ */
+
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ * for Holberton project
+ * @n: Integer
+ * @index: Index of the next in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
